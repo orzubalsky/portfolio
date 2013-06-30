1bf70cd5ab14ba2d713b5d0ba1a03009edbd4fb5
Or Zubalsky <juviley@gmail.com>
2013-06-30 14:14:11 -0400

    implemented generic class views

--- orzubalsky/apps/portfolio/urls.py
+++ orzubalsky/apps/portfolio/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
+from portfolio.views import *
 
 # orzubalskydotcom application
 urlpatterns = patterns('portfolio.views',
     url(r'projects$', TemplateView.as_view(template_name="project_list.html"), name='project-list'),
-    url(r'news/(?P<post_slug>[0-9A-Za-z\-]+)$', 'news_view', name='news-view'),
-    url(r'news$', 'news_list', name='news-list'),    
-    url(r'project/(?P<project_slug>[0-9A-Za-z\-]+)$', 'project_view', name='project-view'),
+    url(r'news/(?P<slug>[0-9A-Za-z\-]+)$', PostDetail.as_view(), name='post-detail'),
+    url(r'news$', PostList.as_view(), name='post-list'),    
+    url(r'project/(?P<slug>[0-9A-Za-z\-]+)$', ProjectDetail.as_view(), name='project-detail'),
     url(r'$', TemplateView.as_view(template_name="home.html"), name='home'),
 )
--- orzubalsky/apps/portfolio/views.py
+++ orzubalsky/apps/portfolio/views.py
@@ -1,42 +1,24 @@
-from django.shortcuts import render_to_response, get_object_or_404
-from django.template import RequestContext
-from django.template.loader import render_to_string
-from django.http import HttpResponseRedirect, HttpResponse, Http404, HttpResponse, HttpResponsePermanentRedirect
-from django.core.urlresolvers import reverse
-from django.utils import simplejson as json
-from django.contrib.flatpages.views import render_flatpage
-from django.utils.translation import ugettext_lazy as _
-from django.db import IntegrityError
-from portfolio.utils import unique_slugify
+from django.views.generic import ListView, DetailView
 from portfolio.models import *
 from portfolio.forms import *
 
 
-def news_list(request):
+class PostList(ListView):
     """
     """
-    posts = Post.objects.filter(is_active=True)
-    
-    return render_to_response('news_list.html',{
-            'posts'    : posts,
-        }, context_instance=RequestContext(request))
+    queryset = Post.objects.filter(is_active=True)
+    template_name = 'post_list.html'
 
 
-def news_view(request, post_slug=None):
+class PostDetail(DetailView):
     """
     """
-    post = get_object_or_404(Post, slug=post_slug)
-
-    return render_to_response('news_view.html',{
-            'post'     : post,
-        }, context_instance=RequestContext(request))
+    model = Post
+    template_name = 'post_detail.html'
 
 
-def project_view(request, project_slug=None):
+class ProjectDetail(DetailView):
     """
     """
-    project  = get_object_or_404(Project, slug=project_slug)
-    
-    return render_to_response('project_view.html',{
-            'project' : project,
-        }, context_instance=RequestContext(request))    
+    model = Project
+    template_name = 'project_detail.html'
