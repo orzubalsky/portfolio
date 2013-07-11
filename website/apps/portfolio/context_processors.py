43ff3c900c9d50ab97d05468a69c4b2ba41c3b4f
orzubalsky <juviley@gmail.com>
2013-07-11 11:48:49 -0400

    fixing analytics script inclusion

--- website/apps/portfolio/context_processors.py
+++ website/apps/portfolio/context_processors.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from portfolio.models import *
 
 def projects(request):
     projects = Project.objects.filter(is_active=True, parent=None)
-    return { 'projects' : projects, }
+    
+    return { 'projects' : projects, 'GOOGLE_ANALYTICS_KEY' : settings.GOOGLE_ANALYTICS_KEY }
